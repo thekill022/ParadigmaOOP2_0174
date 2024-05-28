c68f5a4e243b594fc583bda952e9efb51f61b22b
thekill022 <gibfathoni@gmail.com>
2024-05-28 12:47:09 +0700

    pembuatan class pekerja sebagai pewaris dari non-polymotphy class orang dan pembuatan prosedur untuk memberi nilai dan display data umur

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -10,13 +10,22 @@ public:
     }
 };
 
-//pembuatan class pelajar sebagai pewaris dari non-polymotphy class orang dan pembuatan prosedur untuk memberi nilai dan display data umur
+//pembuatan class pekerja sebagai pewaris dari non-polymotphy class orang dan pembuatan prosedur untuk memberi nilai dan display data umur
+class pelajar : public orang {
+public:
+    pelajar(int pUmur) : orang(pUmur) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+};
+
+//pembuatan class pekerja sebagai pewaris dari non-polymotphy class orang dan pembuatan prosedur untuk memberi nilai dan display data umur
 class pekerja : public orang {
 public:
     pekerja(int pUmur) : orang(pUmur) {
-        cout << "Pekerja dibuat\n" << endl;
+        cout << "Pelajar dibuat\n" << endl;
     }
 };
+
 
 int main()
 {
