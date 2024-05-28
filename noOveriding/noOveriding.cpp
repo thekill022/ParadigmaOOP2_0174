6abfbf66c02e2d1f66d0ae5d4745c7b7401832b2
thekill022 <gibfathoni@gmail.com>
2024-05-28 12:16:57 +0700

    membuat class baseclass, untuk mencegah polymorphism tambahkan final diujung class

---
+++ noOveriding/noOveriding.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//membuat class baseclass, untuk mencegah polymorphism tambahkan final diujung class
+class baseClass //final
+{
+
+};
+
+int main()
+{
+   
+}
