3493774683583f73d3f6d8970ee6a0b0acf9c9b9
thekill022 <gibfathoni@gmail.com>
2024-05-28 12:18:03 +0700

    jika menggunakan final di ujung class, maka pewarisan semacam ini tidak akan bisa

--- noOveriding/noOveriding.cpp
+++ noOveriding/noOveriding.cpp
@@ -4,10 +4,26 @@ using namespace std;
 //membuat class baseclass, untuk mencegah polymorphism tambahkan final diujung class
 class baseClass //final
 {
+public:
+    virtual void perkenalan() {
+        cout << "Hali saya Function dari base class";
+    }
+};
 
+//jika menggunakan final di ujung class, maka pewarisan semacam ini tidak akan bisa
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << "Halo saya Function dari derived class";
+    }
 };
 
 int main()
 {
    
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+
 }
