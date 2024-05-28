cf28be153c1c110b765bb0cd41d98487b79080b3
thekill022 <gibfathoni@gmail.com>
2024-05-28 12:27:58 +0700

    prosedur untuk menampilkan nilai

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -15,6 +15,12 @@ public:
         y = b;
     }
 
+    //prosedur untuk menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+
 };
 
 int main()
