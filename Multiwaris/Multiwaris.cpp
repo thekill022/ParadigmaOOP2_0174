6937637bdc609963be2875d24bfbccf4020e542a
thekill022 <gibfathoni@gmail.com>
2024-05-28 12:37:01 +0700

    pembuatan class pelajar sebagai pewaris dari class orang dan pembuatan prosedur untuk memberi nilai dan display data umur

--- Multiwaris/Multiwaris.cpp
+++ Multiwaris/Multiwaris.cpp
@@ -19,6 +19,14 @@ public:
     }
 };
 
+//pembuatan class pelajar sebagai pewaris dari class orang dan pembuatan prosedur untuk memberi nilai dan display data umur
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) : orang(pUmur) {
+        cout << "Pelajar dibuat" << endl;
+    }
+};
+
 int main()
 {
     
