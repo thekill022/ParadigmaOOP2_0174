9bb43fdc4b8397aacc028181d6c78269a346f12b
thekill022 <gibfathoni@gmail.com>
2024-05-28 12:26:58 +0700

    delarasi variabel string dengan akses modifier private pada class abstraksi

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -4,6 +4,10 @@ using namespace std;
 //membuat class abstraksi
 class abstraksi {
 
+    //delarasi variabel string dengan akses modifier private pada class abstraksi
+private:
+    string x, y;
+
 };
 
 int main()
